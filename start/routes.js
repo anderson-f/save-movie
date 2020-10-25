8ed8493f0d2ceb59a2b943d8fe7a5bd66c1a3837
Anderson Freitas <andersonfreitas.tec@gmail.com>
2020-10-25 13:18:21 -0300

    Versão inicial da API

---
+++ start/routes.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use("Route");
+
+Route.get("/", () => {
+  return { greeting: "Hello world in JSON" };
+});
+
+Route.post("/user/register", "UserController.register");
+Route.post("/user/authenticated", "UserController.authenticated");
+
+Route.group(() => {
+  Route.resource("genre", "GenreController").apiOnly();
+  Route.resource("movies", "MovieController").apiOnly();
+  Route.resource("watched", "WatchedController")
+    .apiOnly()
+    .except(["store", "show", "delete"]);
+}).middleware("auth");
